9eb7e451b18eeb6b7cb069a3af314fe32adc53c5
Flatman01 <deeropro@yandex.ru>
2025-04-21 21:30:24 +0300

    11

--- src/main/java/web/Service/UserService.java
+++ src/main/java/web/Service/UserService.java
@@ -7,7 +7,10 @@ import java.util.List;
 public interface UserService {
 
     List<User> index();
+
     User show(int id);
+
     void save(User user);
+
     void delete(int id);
 }
--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -11,7 +11,6 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     }
 
 
-
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[]{
